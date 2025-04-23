26a35737725365bccabea9d975b09b334699115a
anu <Anuroyw12@gmail.com>
2025-04-23 17:23:51 +0530

    test1

--- money-map/app/(routes)/dashboard/budget/page.jsx
+++ money-map/app/(routes)/dashboard/budget/page.jsx
@@ -5,7 +5,7 @@ import BudgetList from './_components/BudgetList'
 function Budget() {
   return (
     <div className='p-10'> 
-      <h2 className='font-bold text-3xl'>My Budgets</h2>
+      <h2 className='font-bold text-3xl'>My Budget</h2>
       <BudgetList />
     </div>
   )
